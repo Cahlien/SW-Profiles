b4961b0f6465d43e1547d59f717b489c7f0758c3
Matthew Crowell <>
2021-11-08 16:55:09 -0600

    ad character list

---
+++ components/character-list.js
@@ -0,0 +1,37 @@
+import {Fragment} from "react";
+import {Button, Table} from "react-bootstrap";
+import {useSelector} from "react-redux";
+import {useRouter} from "next/router";
+
+export default function CharacterList(){
+    const characters = useSelector(state => state.characters.characters);
+    const router = useRouter();
+
+    async function handleButtonClick(event) {
+        event.preventDefault();
+        await router.push('/characters/' + event.target.value);
+    }
+
+    return (
+        <Fragment>
+            <Table striped bordered hover className={'me-3 table-responsive'} >
+                <thead>
+                <tr>
+                    <th className={'col-11'}>Name</th>
+                    <th className={'col-1'}>Details</th>
+                </tr>
+                </thead>
+                <tbody>
+                {characters && characters.map((character, index) => (
+                <tr key={index}>
+                    <td>{character.name}</td>
+                    <td className={'float-end'}>
+                        <Button onClick={handleButtonClick} value={index}>View</Button>
+                    </td>
+                </tr>
+                ))}
+                </tbody>
+            </Table>
+        </Fragment>
+    )
+}
