6dc2b666ec9f531ad995e4d0420ca997ce80b1dc
Matthew Crowell <>
2021-11-08 18:54:28 -0600

    update species section

--- components/species-section.js
+++ components/species-section.js
@@ -8,7 +8,6 @@ export default function SpeciesSection(props){
     async function fetchSpeciesInformation(){
         const results = await axios.get(url);
         setSpecies(results);
-        console.log(results);
     }
 
     useEffect(() => {
@@ -20,13 +19,22 @@ export default function SpeciesSection(props){
     return (
       <Fragment>
           <h3 className={'mt-3'}>Species Details</h3>
+          {species &&
           <div>
               <p><strong>Name: </strong>{species?.data?.name}</p>
               <p><strong>Classification: </strong>{species?.data?.classification}</p>
               <p><strong>Designation: </strong>{species?.data?.designation}</p>
               <p><strong>Language: </strong>{species?.data?.language}</p>
           </div>
-
+          }
+          {!species &&
+          <div>
+              <p><strong>Name: </strong>Not Listed</p>
+              <p><strong>Classification: </strong>Not Listed</p>
+              <p><strong>Designation: </strong>Not Listed</p>
+              <p><strong>Language: </strong>Not Listed</p>
+          </div>
+          }
       </Fragment>
     );
 }
